73087d1c42a25de2a455c8204f4a420e23715e38
Damir Nurgaliev <damir.nurgaliev@flatstack.com>
2022-09-16 01:43:37 +0200

    Add basic express

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
-import TelegramBot from 'node-telegram-bot-api';
+// import TelegramBot from 'node-telegram-bot-api';
+//
+// const token = process.env['BOT_TOKEN'] || '';
+//
+// const bot = new TelegramBot(token, { polling: true });
+//
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//
+//   bot.sendMessage(chatId, 'Received your message');
+// });
+//
 
-const token = process.env['BOT_TOKEN'] || '';
+import express from 'express';
 
-const bot = new TelegramBot(token, { polling: true });
+const app = express();
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Received your message');
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
 });
 
+// start the server listening for requests
+app.listen(process.env['PORT'], () =>
+  console.log('Server is running...'),
+);
