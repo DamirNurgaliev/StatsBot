9ec407b379327a0684760773fc019ab335abb924
Damir Nurgaliev <damir.nurgaliev@flatstack.com>
2022-09-16 01:22:57 +0200

    Setup base bot

---
+++ app.ts
@@ -0,0 +1,12 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env['BOT_TOKEN'] || '';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Received your message');
+});
+
